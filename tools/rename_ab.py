fcfbc112f8b674bf355c0caeea52e814bb1cab3e
userNOMA <1421232176@qq.com>
2023-08-07 22:41:15 +0800

    2023年08月07日 22:38:17 修改了测试路径

--- tools/rename_ab.py
+++ tools/rename_ab.py
@@ -178,7 +178,7 @@ inf = f'''将批量更改该文件夹下的文件名：
 zxs_print.print(inf, pri=9)
 inp = input("请输入：Y/y确认\n")
 if inp == 'Y' or inp == 'y':
-    rename_ab1(r'D:\Learn\TestFiles\相似图片224组')
-    # rename_ab1(os.getcwd())
+    # rename_ab1(r'D:\Learn\TestFiles\相似图片224组')
+    rename_ab1(os.getcwd())
 else:
     zxs_print.print(f'未确认，退出运行！', pri=9)
